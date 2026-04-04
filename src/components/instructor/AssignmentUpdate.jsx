a3be9d6b96fa158dbadc44b643765e20d787d731
Reece Ram <reecerram@Gmail.com>
2026-04-04 00:31:52 -0700

    instructor list create edit delete assignments Needs better notifications and date input

--- src/components/instructor/AssignmentUpdate.jsx
+++ src/components/instructor/AssignmentUpdate.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react';
-import { GRADEBOOK_URL } from '../../Constants';
+import {GRADEBOOK_URL, REGISTRAR_URL} from '../../Constants';
 import Messages from '../Messages';
 
 const AssignmentUpdate = ({ editAssignment, onClose }) => {
@@ -16,22 +16,56 @@ const AssignmentUpdate = ({ editAssignment, onClose }) => {
     setMessage('');
     setAssignment(editAssignment);
     // to be implemented.  invoke showModal() method on the dialog element.
-    // dialogRef.current.showModal();
+    dialogRef.current.showModal();
   };
 
+  const dialogClose = () => {
+    dialogRef.current.close();
+    onClose();
+  };
 
+  const editChange = (event) => {
+    setAssignment({ ...assignment, [event.target.name]: event.target.value})
+  };
 
-  return (
+  const onSave = async () => {
+    try {
+      const response = await fetch(`${GRADEBOOK_URL}/assignments`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": sessionStorage.getItem("jwt"),
+        },
+        body: JSON.stringify(assignment),
+      });
+      if (response.ok) {
+        const newAssignment = await response.json();
+        setMessage("Assignment " + newAssignment.title + " updated");
+      } else {
+        const body = await response.json();
+        setMessage(body);
+      }
+    } catch (err) {
+      setMessage(String(err));
+    }
+  }
+
+return (
     <>
       <button onClick={editOpen}>Edit</button>
-      <dialog ref={dialogRef} >
-        <p>To be implemented.  Show the id, title and due date of the assignemnt.
-          Allow user to edit the title and due date.
-          Buttons for Close and Save.
-        </p>
+      <dialog ref={dialogRef}>
+        <h2>Edit Course</h2>
+        <Messages response={message} />
+        <input type="text" name="title" value={assignment.title} placeholder="title" onChange={editChange} />
+        <input type="date" name="dueDate" value={assignment.dueDate} placeholder="dueDate" onChange={editChange} />
+        <button onClick={dialogClose}>Close</button>
+        <button onClick={onSave}>Save</button>
       </dialog>
     </>
-  )
+);
 }
 
 export default AssignmentUpdate;
+
+// To be implemented.  Show the id, title and due date of the assignment.
+// Allow user to edit the title and due date. Buttons for Close and Save.
--- src/components/instructor/AssignmentsView.jsx
+++ src/components/instructor/AssignmentsView.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom'
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
-import { GRADEBOOK_URL } from '../../Constants';
+import {GRADEBOOK_URL, REGISTRAR_URL} from '../../Constants';
 import AssignmentAdd from './AssignmentAdd';
 import AssignmentUpdate from './AssignmentUpdate';
 import AssignmentGrade from './AssignmentGrade';
 import Messages from '../Messages';
+import UserUpdate from "../admin/UserUpdate.jsx";
+import UserAdd from "../admin/UserAdd.jsx";
+import CourseUpdate from "../admin/CourseUpdate.jsx";
 
 
 const AssignmentsView = () => {
@@ -46,24 +49,75 @@ const AssignmentsView = () => {
     fetchAssignments()
   }, []);
 
+  const onDelete = (assignmentId) => {
+    confirmAlert({
+      title: "Confirm to delete",
+      message: "Do you really want to delete?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => deleteAssignment(assignmentId),
+        },
+        {
+          label: "No",
+        },
+      ],
+    });
+  };
 
-
+  const deleteAssignment = async (assignmentId) => {
+    try {
+      const response = await fetch(`${GRADEBOOK_URL}/assignments/${assignmentId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": sessionStorage.getItem("jwt"),
+        },
+      });
+      if (response.ok) {
+        setMessage("Assignment deleted");
+        await fetchAssignments();
+      } else {
+        const body = await response.json();
+        setMessage(body);
+      }
+    } catch (err) {
+      setMessage(err);
+    }
+  };
 
   const headers = ['id', 'Title', 'Due Date', '', '', ''];
 
-  return (
-    <div>
+return (
+    <>
+      <h3>Assignment View</h3>
       <Messages response={message} />
-
-      <p>To be implemented. Display a table. Column headings are as givin in headers.
-        For each row, show the id, title, due date of the assignment
-        along with buttons to edit and delete the assignment </p>
-
-
-
+      <table className="Center">
+        <thead>
+        <tr>
+          {headers.map((s, idx) => (
+              <th key={idx}>{s}</th>
+          ))}
+        </tr>
+        </thead>
+        <tbody>
+        {assignments.map((assignment) => (
+            <tr key={assignment.id}>
+              <td>{assignment.id}</td>
+              <td>{assignment.title}</td>
+              <td>{assignment.dueDate}</td>
+              <td><AssignmentUpdate editAssignment={assignment} onClose={fetchAssignments} /></td>
+              <td><button onClick={() => onDelete(assignment.id)}>Delete</button></td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
       <AssignmentAdd secNo={secNo} onClose={fetchAssignments} />
-    </div>
-  );
+    </>
+);
 }
-
 export default AssignmentsView;
+
+// To be implemented. Display a table. Column headings are as given in headers.
+// For each row, show the id, title, due date of the assignment
+// along with buttons to edit and delete the assignment
